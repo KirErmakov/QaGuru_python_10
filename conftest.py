5b9899781056723d1161cde1ef525abb0d3ecdc9
Kirill Ermakov <lvstrng161@gmail.com>
2024-01-11 21:36:06 +0400

    conftest and requirements were updated

--- conftest.py
+++ conftest.py
@@ -4,6 +4,6 @@ from selene import browser
 @pytest.fixture(scope="function", autouse=True)
 def browser_setup():
     browser.config.window_width = 1080
-    browser.config.window_height = 800
+    browser.config.window_height = 850
     yield
     browser.quit()
